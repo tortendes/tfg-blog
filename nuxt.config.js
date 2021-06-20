a2adb6d35bf04b78b73ad1e5523f09848e0e1abb
Teco The Taco <texo@furrygalaxy.xyz>
2021-06-20 17:36:00 +0800

    added a custom favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,7 +10,7 @@ export default {
       { hid: 'description', name: 'description', content: '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'icon', type: 'image/png', href: '/favicon.png' },
       { rel: 'preconnect', href: 'https://fonts.gstatic.com' },
       { href: 'https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;500;600;700;800;900&display=swap', rel: 'stylesheet' },
       { href: 'https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;500;600;700;800;900&family=Merriweather:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&display=swap', rel: 'stylesheet' }
